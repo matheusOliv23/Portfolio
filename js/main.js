a4b6be7b6571d0c443bdef786eb16abe2c789734
matheusOliv23 <matheushenri1996@gmail.com>
2021-02-01 19:23:26 -0300

    Header completo

--- js/main.js
+++ js/main.js
@@ -3,10 +3,18 @@ function typeWriter(){
     titulo.innerHTML = '';
     textoArray.forEach((letra, i)=>{
         setTimeout(() =>
-            titulo.innerHTML += letra, 160 * i)  
+            titulo.innerHTML += letra, 180 * i)  
             console.log(letra);      
         
     });
 }
 const titulo = document.querySelector('h1'); /* pega o título do meu banner*/
 typeWriter(titulo);
+
+
+let hamburguer = document.querySelector('.hamburguer');
+
+
+hamburguer.addEventListener('click',()=> 
+    document.querySelector('.container').classList.toggle('mostrar-menu')
+);
